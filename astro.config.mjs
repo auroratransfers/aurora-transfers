9dc463ce93e2f42f8bd11fa3c7db9c6bd4ed0a4d
Aurora Transfers <info@aurora-transfers.com>
2026-08-28 21:54:33 +0200

    Use Aurora Transfers production domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://aurora.vercel.app",
+  site: "https://aurora-transfers.vercel.app",
   output: "static",
   trailingSlash: "always",
   integrations: [sitemap()]
