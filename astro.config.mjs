65673f6b58550634273fbca5a7c8ecf17513866d
Aurora Transfers <info@aurora-transfers.com>
2026-08-28 21:52:59 +0200

    Serve Aurora directly from root URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://auroratransfers.com",
+  site: "https://aurora.vercel.app",
   output: "static",
   trailingSlash: "always",
   integrations: [sitemap()]
